580bab91edb60e59b4b72fdde1516f74fe52801c
Jacob Peltola <jtpup0@aim.com>
2014-10-18 11:28:03 -0400

    Yolks and Stuff
    -added art
    -rudimentary rigidbody programming
    
    in need of scoring and better art. (and maybe a unique mechanic)

---
+++ Assets/Scripts/BaconBrickScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaconBrickScript : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+    void Update()
+    {
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, mousePosition.y);
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.tag == "egg")
+        {
+            col.gameObject.SendMessage("RefreshSpeed");
+        }
+    }
+}
---
+++ Assets/Scripts/EggScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EggScript : MonoBehaviour {
+
+    public GameObject yolkandstuff;
+
+    public float speed;
+    public float durability = 100;
+
+    SpriteRenderer eggSprite;
+
+    public Sprite crack1;
+    public Sprite crack2;
+    public Sprite crack3;
+
+	// Use this for initialization
+	void Start () {
+        eggSprite = gameObject.GetComponent<SpriteRenderer>();
+
+        speed = Random.Range(2F, 5F);
+        rigidbody2D.velocity = Vector2.one.normalized * speed;
+	}
+
+    void Update()
+    {
+        if (durability < 75)
+        {
+            eggSprite.sprite = crack1;
+        }
+        if (durability < 50)
+        {
+            eggSprite.sprite = crack2;
+        }
+        if (durability < 25)
+        {
+            eggSprite.sprite = crack3;
+        }
+        if (durability <= 0)
+        {
+            Instantiate(yolkandstuff, transform.position, transform.rotation);
+            Destroy(gameObject, 5);
+        }
+    }
+
+    void RefreshSpeed()
+    {
+        Debug.Log("egghit");
+        durability -= Random.Range(10F, 15F);
+        speed = 5;
+    }
+}
