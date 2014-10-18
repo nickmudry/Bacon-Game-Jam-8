5b43fdfa811e58338f7218250d9b5cc8e6dda606
Jacob Peltola <jtpup0@aim.com>
2014-10-18 13:59:08 -0400

    Incomplete ScoreBoard and Egg cracking
    yeah

--- Assets/Scripts/EggScript.cs
+++ Assets/Scripts/EggScript.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class EggScript : MonoBehaviour {
 
     public GameObject yolkandstuff;
+    public GameObject sm;
 
-    public float speed;
+    public float speed = 10;
     public float durability = 100;
 
     SpriteRenderer eggSprite;
@@ -17,8 +18,6 @@ public class EggScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         eggSprite = gameObject.GetComponent<SpriteRenderer>();
-
-        speed = Random.Range(2F, 5F);
         rigidbody2D.velocity = Vector2.one.normalized * speed;
 	}
 
@@ -38,8 +37,12 @@ public class EggScript : MonoBehaviour {
         }
         if (durability <= 0)
         {
-            Instantiate(yolkandstuff, transform.position, transform.rotation);
-            Destroy(gameObject, 5);
+            GameObject clone;
+            clone = (GameObject)Instantiate(yolkandstuff, transform.position, transform.rotation);
+            clone.transform.Rotate(new Vector3(0, 0, Random.Range(-360F, 360F)));
+            clone.rigidbody2D.AddForce(new Vector2(Random.Range(-5F, 5F), Random.Range(-5F, 5F) * speed));
+            Destroy(clone.gameObject, 1F);
+            Destroy(gameObject, 2F);
         }
     }
 
@@ -47,6 +50,18 @@ public class EggScript : MonoBehaviour {
     {
         Debug.Log("egghit");
         durability -= Random.Range(10F, 15F);
-        speed = 5;
+        speed = 10;
+    }
+
+    void OnTriggerEnter(Collider trg)
+    {
+        if (trg.collider.tag == "player2scores")
+        {
+            sm.SendMessage("P2Increase");
+        }
+        if (trg.collider.tag == "player1scores")
+        {
+            sm.SendMessage("P1Increase");
+        }
     }
 }
---
+++ Assets/Scripts/ScoreManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManagerScript : MonoBehaviour {
+    public float player1score;
+    public float player2score;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        guiText.text = player1score + " | " + player2score;
+
+	}
+
+    void P1Increase()
+    {
+        Debug.Log("P1scores");
+        player1score += 1;
+    }
+
+    void P2Increase()
+    {
+        player2score += 1;
+    }
+}
