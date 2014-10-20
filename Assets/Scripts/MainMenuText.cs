19399319142896325c0764669dbedcd2839b6d7a
Nick Mudry <Nick@Nicholass-MacBook-Air.local>
2014-10-19 21:08:51 -0400

    Main Menu Started
    Started work on a temporary main menu.

---
+++ Assets/Scripts/MainMenuText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		guiText.text = "Bacon Duel \n" +
+						"Press 'Space' To Start! \n" +
+						"\n" +
+						"Credits: \n" +
+						"Jacob Peltola - Code, Art, Sound \n" +
+						"Nick Mudry - Code, Game Design";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Application.LoadLevel("test");
+		}
+	}
+}
